6eec1b19c679bdb95d6362b071d25c9f7e4ff054
fl-sll <edwardalvin@gmail.com>
2023-11-29 08:48:28 +0700

    adjusted label size, included timer and memory counter

--- PT.py
+++ PT.py
@@ -2,6 +2,8 @@ import numpy as np
 from Bio import pairwise2
 from scipy.cluster import hierarchy
 import matplotlib.pyplot as plt
+import time
+import tracemalloc
 
 def smith_waterman_distance(seq1, seq2):
     #? m = match score of identical chars
@@ -13,34 +15,43 @@ def smith_waterman_distance(seq1, seq2):
     score = alignment[0].score
     return len(seq1) + len(seq2) - score * 2
 
-#// Example sequences
-sequences = [
-    "ACCGTACGGA",
-    "ACC-ACAGGA",
-    "ACCGTA--GA",
-    "AGTACGCA",
-    "TATGC",
-    "AGTACCGA",
-    "AGTACCCA"
-]
+def main():
+    tracemalloc.start()
+    start = time.time()
+    #// Example sequences
+    sequences = [
+        "ACCGTACGGA",
+        "ACC-ACAGGA",
+        "ACCGTA--GA",
+        "AGTACGCA",
+        "TATGC",
+        "AGTACCGA",
+        "AGTACCCA"
+    ]
 
-num_seqs = len(sequences)
-distance_matrix = np.zeros((num_seqs, num_seqs))
+    num_seqs = len(sequences)
+    distance_matrix = np.zeros((num_seqs, num_seqs))
 
-for i in range(num_seqs):
-    for j in range(i + 1, num_seqs):
-        distance = smith_waterman_distance(sequences[i], sequences[j])
-        distance_matrix[i][j] = distance
-        distance_matrix[j][i] = distance
+    for i in range(num_seqs):
+        for j in range(i + 1, num_seqs):
+            distance = smith_waterman_distance(sequences[i], sequences[j])
+            distance_matrix[i][j] = distance
+            distance_matrix[j][i] = distance
 
-#// Perform hierarchical clustering (UPGMA)
-link = hierarchy.linkage(distance_matrix, method='average')
+    #// Perform hierarchical clustering (UPGMA)
+    link = hierarchy.linkage(distance_matrix, method='average')
+    end = time.time()
+    mem = tracemalloc.get_traced_memory()
+    print(f"Time = {end-start}")
+    print(f"memory = {mem[0]}")
 
-#// Plot the phylogenetic tree
-#// https://codinginfinite.com/plot-dendrogram-in-python/
-plt.figure(figsize=(8, 6))
-dendrogram = hierarchy.dendrogram(link, labels=sequences, leaf_rotation=-90)
-plt.xlabel('Sequence Index')
-plt.ylabel('Distance')
-plt.title('Phylogenetic Tree')
-plt.show()
+    #// Plot the phylogenetic tree
+    #// https://codinginfinite.com/plot-dendrogram-in-python/
+    plt.figure(figsize=(8, 6))
+    dendrogram = hierarchy.dendrogram(link, labels=sequences, leaf_rotation=-75, leaf_font_size=6)
+    plt.xlabel('Sequence Index')
+    plt.ylabel('Distance')
+    plt.title('Phylogenetic Tree')
+    plt.show()
+
+main()
